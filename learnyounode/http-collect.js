eb21d31affe587f28d999804a400b5d8ca0bc506
Udayraj Mandke <urmandke@gmail.com>
2020-09-23 13:11:50 +0530

    WIP: http-collect not working yet

--- learnyounode/http-collect.js
+++ learnyounode/http-collect.js
@@ -5,13 +5,18 @@ const address = process.argv[2];
 let str = "";
 
 
-http.get(address, function callback(response){
-   (response.end('data', qfunction({
-        response.on('data', function (data){
+http.get(address, function callback(response)
+{
+   while(!(response.on('end',function(){return}))){
+
+    response.on('data',function callback2(data){
+        console.log(data);        
         str += data;
-        })
-    })
-})
+        }
+      )
+    }
+});
+   
 
 console.log(str.length);
 console.log(str); 
